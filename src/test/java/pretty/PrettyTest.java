fddcf454c19b30e1f7f3ca55acc12eeb00205a21
davepmiller <dpmdavemiller@gmail.com>
2019-07-17 11:36:28 -0600

    Enable travis-ci

--- src/test/java/pretty/PrettyTest.java
+++ src/test/java/pretty/PrettyTest.java
@@ -5,9 +5,12 @@ package pretty;
 
 import java.util.function.Function;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnit4.class)
 public class PrettyTest {
     private static String TEST_STRING = "testString";
 
